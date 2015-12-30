44de8efab587ffe74f87e297e1445af8ed214b7a
Hernan Costante <hernancostante@gmail.com>
2015-12-30 08:34:30 -0300

    cambios de nombre en los files

--- clarin.js
+++ clarin.js
@@ -9,7 +9,3 @@ document.querySelector('div.modalLoginPase').style.display = 'none';
 //Make Y scrollbar visible
 document.querySelector('html').style.overflow = 'auto';
 
-
-// La Nacion
-document.querySelector('div.lnmodal pantalla-completa login').style.display = 'none';
-//diy class="lnmodal pantalla-completa login"
---
+++ lanacion.js
@@ -0,0 +1,17 @@
+// La Nacion
+
+//document.querySelector('iframe#iframe-registracion').style.display = 'none';
+//document.querySelector('div.info').style.display = 'none';
+//diy class="lnmodal pantalla-completa login"
+
+//function removeElementsByClassName(names) {
+//    var els = document.getElementsByClassName(names),
+//        i, element;
+//    for (i = els.count - 1; i > 0; i -= 1) {
+//        element = els[i];
+//        element.parentElement.removeChild(element);
+//    }
+//}
+//
+//removeElementsByClassName('div.container');
+
